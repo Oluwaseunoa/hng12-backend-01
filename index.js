04a5cd4c445bcf2cf334f90232e8c3c724d70cef
Oluwaseunoa <oluwaseun.beicks@gmail.com>
2025-01-28 22:26:41 +0100

    Finished the task

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-import express from express;
-import cors from cors;
+import express from "express";
+import cors from "cors";
 
 const app = express();
-
+const PORT = process.env.PORT || 3000;
+app.use(cors())
 
 app.get("/", (req, res) => {
-    
-})
+    const response = {
+        email: "oluwaseun.beicks@gmail.com",
+        current_datetime: new Date().toISOString(),
+        github_url: 
+        "https://github.com/Oluwaseunoa/hng12-backend-01"
+    }
+    res.status(200).json(response)
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
